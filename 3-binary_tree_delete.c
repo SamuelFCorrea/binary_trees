fc8b4d007f629f04be70b43bbe171d4fdaab543b
SamuelFCorrea <70902050+SamuelFCorrea@users.noreply.github.com>
2021-03-11 13:19:13 -0500

    Update 3-binary_tree_delete.c

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -9,39 +9,10 @@
 
 void binary_tree_delete(binary_tree_t *tree)
 {
-	binary_tree_t *aux, *parent;
-
 	if (!tree)
 		return;
 
-	parent = tree;
-
-	/* infinite bucle */
-	while (1)
-	{
-		/* Walk trought the tree */
-		if (parent->left)
-			parent = parent->left;
-		else if (parent->right)
-			parent = parent->right;
-		/* If the node is a leave delete it */
-		else
-		{
-			aux = parent;
-			parent = parent->parent;
-			/* Disconect the node */
-			if (parent->left)
-				parent->left = NULL;
-			else
-				parent->right = NULL;
-			/* Free it */
-			free(aux);
-		}
-		/* If 'parent' is the root node end the bucle */
-		if (parent == tree && (!tree->right && !tree->left))
-		{
-			free(tree);
-			return;
-		}
-	}
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
 }
